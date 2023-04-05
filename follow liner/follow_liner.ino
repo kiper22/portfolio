457ac1562e2a22b33036ec33c426813634c6fb51
kiper22 <125763668+kiper22@users.noreply.github.com>
2023-04-05 21:19:35 +0200

    Update follow_liner.ino

--- follow liner/follow_liner.ino
+++ follow liner/follow_liner.ino
@@ -1,5 +1,8 @@
+// Copyright (c) 2023, Kacper Wieleba
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to use the Software for private, non-commercial purposes only, subject to the following conditions:
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-// deklaracja pinów dla czujników odbiciowych
 const int leftSensorPin = A0; int LSP = 0;
 const int centerLeftSensorPin = A1; int CLSP = 0;
 const int centerRightSensorPin = A2; int CRSP = 0;
@@ -10,22 +13,18 @@ const int motorDirection2 = 8; int MD2 = 0;
 const int motorSpeed1 = 9; int MS1 = 0;
 const int motorSpeed2 = 10; int MS2 = 0;
 
-//const int buttonPin = 4;
-//int buttonState = 0;
-//int velOnOff = 0;
-
 int deviationFromCenter() {
-  int leftSensorValue = analogRead(A0); // odczyt wartości z lewego czujnika
-  int rightSensorValue = analogRead(A3); // odczyt wartości z prawego czujnika
-  int centerValue = (leftSensorValue + rightSensorValue) / 2; // środkowa wartość
+  int leftSensorValue = analogRead(A0);
+  int rightSensorValue = analogRead(A3);
+  int centerValue = (leftSensorValue + rightSensorValue) / 2;
 
-  int deviation = (centerValue - leftSensorValue); // obliczenie odchylenia od lewego czujnika
+  int deviation = (centerValue - leftSensorValue);
 
-  return deviation; // zwrócenie wartości odchylenia
+  return deviation; // feedback - deviation
 }
 
 void setup() {
-  // konfiguracja pinów dla czujników odbiciowych jako wejścia analogowe
+  // pin configuration
   pinMode(leftSensorPin, INPUT);
   pinMode(centerLeftSensorPin, INPUT);
   pinMode(centerRightSensorPin, INPUT);
@@ -36,34 +35,24 @@ void setup() {
   pinMode(motorSpeed1, OUTPUT);
   pinMode(motorSpeed2, OUTPUT);
 
-//  pinMode(buttonPin, INPUT_PULLUP);
-  // włączenie monitora portu szeregowego
   Serial.begin(9600);
 }
 
 void loop() {
-  // odczyt wartości z czujników odbiciowych
+  // reacding values from sensors
   int LSP = analogRead(leftSensorPin);
   int CLSP = analogRead(centerLeftSensorPin);
   int CRSP = analogRead(centerRightSensorPin);
   int RSP = analogRead(rightSensorPin);
-
-//  buttonState = digitalRead(buttonPin);
-//  if (buttonState == HIGH){
-//    velOnOff = 1;
-//  }
-//  else{
-//    velOnOff = 0;
-//  }
   
   int deviation = deviationFromCenter();
   Serial.print("Deviation: ");
   Serial.println(deviation);
 
-  // wywołanie funkcji sterującej silnikami z regulatorem P
+  // P regulator
   int error = deviation/17;
-  int Kp = 1; // wzmocnienie regulatora P
-  int speed = 45; // prędkość obrotowa silników
+  int Kp = 1;
+  int speed = 45;
   int rightSpeed = (-speed + (error * Kp));
   int leftSpeed = (-speed - (error * Kp));
 
@@ -86,7 +75,7 @@ void loop() {
   analogWrite(motorSpeed1, leftSpeed);
   analogWrite(motorSpeed2, rightSpeed);
 
-  // wypisanie wartości na monitorze szeregowym
+  // feedback in serial monitor
   Serial.print("Left sensor value: ");
   Serial.print(LSP);
   Serial.print(" | Center sensor value: ");
@@ -96,7 +85,6 @@ void loop() {
   Serial.print(" | Fourth sensor value: ");
   Serial.print(RSP);
   Serial.println();
-
-  // opóźnienie pomiędzy odczytami
-  delay(10);
+  
+  delay(50);
 }
